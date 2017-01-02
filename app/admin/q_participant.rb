15dc9aa406ba6b49036c9906f595f125527a1ea4
Murugan <murugu2012@gmail.com>
2017-01-02 20:03:48 +0530

    Whitelist columns in QParticipant for ActiveAdmin

--- app/admin/q_participant.rb
+++ app/admin/q_participant.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register QParticipant do
 
+ permit_params :user_id, :status, :host_status, :queue_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
