53e34766db92fa550b7da44f78a8f32132d0f92a
Murugan <murugu2012@gmail.com>
2017-01-02 20:03:13 +0530

    Direct association between users and votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :votes,
+             :dependent => :destroy
+
   has_many   :suggestions,
              :dependent => :destroy
 
