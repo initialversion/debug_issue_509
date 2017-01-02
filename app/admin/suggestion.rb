80ac9a2b6c0007d8005decd2f09a8abb68568cc9
Murugan <murugu2012@gmail.com>
2017-01-02 20:03:50 +0530

    Whitelist columns in Suggestion for ActiveAdmin

--- app/admin/suggestion.rb
+++ app/admin/suggestion.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Suggestion do
 
+ permit_params :user_id, :song_id, :up_vote_count, :down_vote_count, :status, :qlineup_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
