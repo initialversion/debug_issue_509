26637b15ac31608bee4d7e6c86d7cb0297e3602a
Murugan <murugu2012@gmail.com>
2017-01-02 20:03:13 +0530

    Direct association between q_participants and q_invitations

--- app/models/q_invitation.rb
+++ app/models/q_invitation.rb
@@ -1,6 +1,9 @@
 class QInvitation < ApplicationRecord
   # Direct associations
 
+  belongs_to :sender,
+             :class_name => "QParticipant"
+
   belongs_to :queue
 
   belongs_to :recipient,
--- app/models/q_participant.rb
+++ app/models/q_participant.rb
@@ -1,6 +1,10 @@
 class QParticipant < ApplicationRecord
   # Direct associations
 
+  has_many   :q_invitations,
+             :foreign_key => "sender_id",
+             :dependent => :destroy
+
   belongs_to :q,
              :class_name => "Queue",
              :foreign_key => "queue_id"
