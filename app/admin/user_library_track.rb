76c1fdb2106489e94e324f1c519947308d5f8882
Murugan <murugu2012@gmail.com>
2017-01-02 20:04:11 +0530

    Whitelist columns in UserLibraryTrack for ActiveAdmin

--- app/admin/user_library_track.rb
+++ app/admin/user_library_track.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register UserLibraryTrack do
 
+ permit_params :song_id, :song_name, :song_artist, :song_attributes, :number_of_listens, :genre, :last_listened_to_date, :song_release_date, :liked
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
